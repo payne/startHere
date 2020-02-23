f13ea1217339ed17bfb8920ca2382e228abbb690
Matt Payne <Payne@MattPayne.org>
2020-02-22 20:16:26 -0600

    Using RowMapper instead of a lambda.

--- src/main/java/com/example/validatingforminput/BooksDao.java
+++ src/main/java/com/example/validatingforminput/BooksDao.java
@@ -1,10 +1,13 @@
 package com.example.validatingforminput;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 
 public class BooksDao {
     private static final Logger log = LoggerFactory.getLogger(BooksDao.class);
@@ -21,10 +24,24 @@ public class BooksDao {
 
         List<Book> lstBooks = jdbcTemplate.query(
                 "SELECT  bookid, title, author, published, stock FROM books ",
-                (rs, rowNum) -> new Book(rs.getInt("bookid"), rs.getString("title"), rs.getString("author"),
-                        rs.getString("published"), rs.getInt("stock")));
+                new BookRowMapper());
         return lstBooks;
     }
+
+    // https://www.baeldung.com/spring-jdbc-jdbctemplate helped
+    class BookRowMapper implements RowMapper<Book> {
+        @Override
+        public Book mapRow(ResultSet rs, int rowNum) throws SQLException {
+            Book book = new Book();
+            book.setBookid(rs.getInt("BOOKID"));
+            book.setTitle(rs.getString("TITLE"));
+            book.setAuthor(rs.getString("AUTHOR"));
+            book.setPublished(rs.getString("PUBLISHED"));
+            book.setStock(rs.getInt("STOCK"));
+            return book;
+        }
+    }
+
 }
 
 
